59cbc7f21c8e00f9c8c65fe17e60516ea5f26255
vijayashanthi1234 <160800323+vijayashanthi1234@users.noreply.github.com>
2026-05-29 19:50:12 +0530

    Implement escrow functionality with createEscrow method
    Added EscrowItem struct and createEscrow function to manage deposits.

--- contracts/ShieldEscrow.sol
+++ contracts/ShieldEscrow.sol
@@ -5,18 +5,49 @@ contract ShieldEscrow {
     address public admin;
     bool public isPaused;
     uint256 public totalEscrowsCreated;
+    
+    struct EscrowItem {
+        address sender;
+        address payable receiver;
+        uint256 amount;
+        bool isReleased;
+    }
+    
+    mapping(uint256 => EscrowItem) public escrows;
     mapping(address => uint256) public lockedBalances;
 
     constructor() {
         admin = msg.sender;
     }
 
+    // Creates an automated deposit vault
+    function createEscrow(address payable _receiver) external payable {
+        require(msg.value > 0, "Must send test funds to escrow");
+        totalEscrowsCreated++;
+        
+        escrows[totalEscrowsCreated] = EscrowItem({
+            sender: msg.sender,
+            receiver: _receiver,
+            amount: msg.value,
+            isReleased: false
+        });
+        
+        lockedBalances[msg.sender] += msg.value;
+    }
+
+    // Releases funds to the receiver
+    function releaseEscrow(uint256 _id) external {
+        EscrowItem storage item = escrows[_id];
+        require(msg.sender == item.sender || msg.sender == admin, "Not authorized");
+        require(!item.isReleased, "Already released");
+        
+        item.isReleased = true;
+        lockedBalances[item.sender] -= item.amount;
+        item.receiver.transfer(item.amount);
+    }
+
     function togglePause() external {
         require(msg.sender == admin, "Only admin can call this");
         isPaused = !isPaused;
-    }
-
-    function getLockedBalance(address _user) external view returns (uint256) {
-        return lockedBalances[_user];
     }
 }
